4958f6cd066ba59abb5628d2b6b714b34f4f0aa9
Daniel Vuk <vuk.daniel@gmail.com>
2023-04-03 01:45:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
